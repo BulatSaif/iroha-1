0e35d1e9041ac6216bb83cae87bd95da9445b3be
Bulat Saifullin <bulat@saifullin.ru>
2020-02-25 19:53:56 +0300

    fix2
    Signed-off-by: Bulat Saifullin <bulat@saifullin.ru>

--- .jenkinsci/builders/x64-mac-build-steps.groovy
+++ .jenkinsci/builders/x64-mac-build-steps.groovy
@@ -37,8 +37,6 @@ def testSteps(scmVars, String buildDir, List environment, String testList) {
 def buildSteps(int parallelism, List compilerVersions, String build_type, boolean coverage, boolean testing, String testList,
        boolean packagebuild, boolean fuzzing, boolean useBTF, List environment) {
   withEnv(environment) {
-    stage('Prepare Mac environment') {
-    scmVars = checkout scm
     def build = load '.jenkinsci/build.groovy'
     def vars = load ".jenkinsci/utils/vars.groovy"
     def utils = load ".jenkinsci/utils/utils.groovy"
@@ -50,7 +48,8 @@ def buildSteps(int parallelism, List compilerVersions, String build_type, boolea
     if (packagebuild){
       cmakeBuildOptions = " --target package "
     }
-
+    stage('Prepare Mac environment') {
+    scmVars = checkout scm
     utils.ccacheSetup(5)
 
     if (!fileExists("/opt/dependencies/vcpkg-1.1.x/scripts/buildsystems/vcpkg.cmake")) {
--- .jenkinsci/builders/x64-win-build-steps.groovy
+++ .jenkinsci/builders/x64-win-build-steps.groovy
@@ -16,7 +16,7 @@ def buildSteps(int parallelism, List compilerVersions, String buildType, boolean
       powershell """
         if(!(Test-Path C:\\vcpkg-1.1.x\\scripts\\buildsystems\\vcpkg.cmake))
         {
-          Remove-Item 'C:\\vcpkg-1.1.x' -Recurse -Force
+          if (Test-Path 'C:\\vcpkg-1.1.x' ) { Remove-Item 'C:\\vcpkg-1.1.x' -Recurse; }
           Add-Content c:\\vcpkg-map.txt "${scmVars.GIT_LOCAL_BRANCH} start  build C:\\vcpkg-1.1.x..."
           .\\.packer\\win\\scripts\\vcpkg.ps1 -vcpkg_path "C:\\vcpkg-1.1.x" -iroha_vcpkg_path "${env.WORKSPACE}\\vcpkg"
           Add-Content c:\\vcpkg-map.txt "${scmVars.GIT_LOCAL_BRANCH} finish build C:\\vcpkg-1.1.x"
